eeb009d280df0ab55d1ff3ee068c4fbdc7ace9f3
muhammad-akif <muhammadakif2917@gmail.com>
2021-12-25 21:16:53 +0500

    api setup

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Search from '../components/home/Search'
 import { View, TextInput, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
 import { useFonts, Inter_900Black, Inter_800ExtraBold } from '@expo-google-fonts/inter';
@@ -12,18 +12,33 @@ export default function Home() {
         Inter_900Black,
         Inter_800ExtraBold
     });
-    if (!fontsLoaded) {
-        return <AppLoading />;
-    }
-    else {
-        return (
-            <View
-                style={styles.screen}
-            >
-                <Search />
-            </View>
-        )
-    }
+
+
+    useEffect( async () => {
+        const responce = await fetch(`https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple`, {
+            method: 'GET',
+            headers: {
+                "x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com",
+                "x-rapidapi-key": "c21f427b09msh296727ad4bf3af7p1dfe3djsned4f6f14e075"
+            }
+        })
+        const data = await responce.json();
+        console.log("resultsss ===> ", data);
+    }, [])
+
+
+
+
+if (!fontsLoaded) {
+    return <AppLoading />;
+}
+else {
+    return (
+        <View style={styles.screen}>
+            <Search />
+        </View>
+    )
+}
 }
 
 const styles = StyleSheet.create({
