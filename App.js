5d7a6832445a980f3508503d77b4f709c361c1a2
muhammad-akif <muhammadakif2917@gmail.com>
2021-12-25 18:51:41 +0500

    structured Navigations

--- App.js
+++ App.js
@@ -1,26 +1,7 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
+import Home from './screens/Home';
 
 const Drawer = createDrawerNavigator();
 
@@ -28,8 +9,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        <Drawer.Screen name="Home" component={Home} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import { Button, View } from 'react-native';
+
+export default function Home({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {/* <Button
+          onPress={() => navigation.navigate('Notifications')}
+          title="Go to notifications"
+        /> */}
+      </View>
+    );
+  }
