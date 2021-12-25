1b744f205d835ab8dcb83f84e8ba66e9d09969d7
muhammad-akif <muhammadakif2917@gmail.com>
2021-12-25 21:12:38 +0500

    seach UI completed

---
+++ components/home/Search.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/EvilIcons';
+
+export default function Search() {
+
+    const [query, setQuery] = useState('')
+    const [focus, setFocus] = useState(false)
+    return (
+        <View style={focus ? styles.focusSearch : styles.search}>
+            <Icon name="search" style={{ alignSelf: 'center' }} size={25} color="grey" />
+            <TextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                clearButtonMode="always"
+                value={query}
+                onFocus={() => setFocus(true)}
+                underlineColorAndroid="transparent"
+                selectionColor="#5956E9"
+                onChangeText={queryText => setQuery(queryText)}
+                placeholder="What are you Looking for?"
+                style={styles.inputText}
+
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    search: {
+        flexDirection: "row",
+        backgroundColor: '#fff',
+        padding: 5,
+        margin: 10,
+        borderRadius: 5
+    },
+    focusSearch: {
+        flexDirection: "row",
+        backgroundColor: '#fff',
+        padding: 5,
+        margin: 10,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: "#5956E9",
+    },
+    inputText: {
+        backgroundColor: '#fff',
+        paddingHorizontal: 10,
+    },
+})
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,49 @@
-import { Button, View } from 'react-native';
+import React from 'react'
+import Search from '../components/home/Search'
+import { View, TextInput, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import { useFonts, Inter_900Black, Inter_800ExtraBold } from '@expo-google-fonts/inter';
+import AppLoading from 'expo-app-loading';
+// import Constants from 'expo-constants';
+// import { LinearGradient } from 'expo-linear-gradient';
 
-export default function Home({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        {/* <Button
-          onPress={() => navigation.navigate('Notifications')}
-          title="Go to notifications"
-        /> */}
-      </View>
-    );
-  }
+const { width } = Dimensions.get('window');
+export default function Home() {
+    let [fontsLoaded] = useFonts({
+        Inter_900Black,
+        Inter_800ExtraBold
+    });
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+    else {
+        return (
+            <View
+                style={styles.screen}
+            >
+                <Search />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        // flex: 1,
+        // justifyContent: 'space-between',
+        // marginTop: Constants.statusBarHeight,
+        // backgroundColor: 'blue',
+        // padding: 42
+    },
+    text: {
+        fontSize: 60,
+        fontFamily: 'Inter_800ExtraBold',
+        color: 'white'
+    },
+    button: {
+        backgroundColor: 'white',
+        height: 50,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
