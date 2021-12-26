fdab9fa0efaae09a5b0921f3f53e1b0329f9a1ed
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-12-26 20:14:02 +0500

    Designed Second

---
+++ components/Card.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, View, StyleSheet, Dimensions, Image, TouchableOpacity } from 'react-native';
+import colors from '../constants/colors';
+
+const { height } = Dimensions.get('window');
+
+const Card = props => { //text | image
+    return (
+        <TouchableOpacity style={[styles.card, props.style]} onPress={props.onPress}>
+            <View style={{ flex: 0.8, backgroundColor: colors.primary, justifyContent: 'center' }}>
+                <Image source={props.image} style={{ width: '100%', height: '70%' }} resizeMode={'contain'} />
+            </View>
+            <View style={{ flex: 0.2, alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={{ fontSize: 16, letterSpacing: 0.3, top: -1, color: 'grey', fontFamily: 'italic' }} numberOfLines={1} adjustsFontSizeToFit={true}>
+                    {props.text}
+                </Text>
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+export default Card;
+
+const styles = StyleSheet.create({
+    card: {
+        height: height / 5.8,
+        width: '100%',
+        borderRadius: 5,
+        backgroundColor: colors.secondary,
+        elevation: 5,
+        overflow: 'hidden'
+    }
+});
--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -4,6 +4,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import Button from '../../components/Button';
 import Steps from '../../components/Steps';
 import Constants from 'expo-constants';
+import Card from '../../components/Card';
 
 const Login = props => {
     return (
@@ -12,36 +13,42 @@ const Login = props => {
                 <Image source={require('../../assets/images/rules.png')} style={{ width: 70, height: 70, marginHorizontal: 18 }} resizeMode="contain" />
             </View>
             <View style={{ flex: 1 }}>
-                <ScrollView contentContainerStyle={{ justifyContent: 'flex-start', marginHorizontal: 20 }}>
-                    <Image />
-                    <View style={{ marginBottom: 10 }}>
-                        <Text style={{ fontWeight: 'bold', fontSize: 20, color: 'black' }}>
-                            Use your search skills
-                        </Text>
-                    </View>
-                    <View style={{ marginBottom: 50 }}>
-                        <Text style={{ letterSpacing: -0.2, fontSize: 14, color: 'grey' }} adjustsFontSizeToFit={true} numberOfLines={1}>
-                            We'll get you the products. We'll make shopping easy.
-                        </Text>
+                <ScrollView contentContainerStyle={{ justifyContent: 'flex-start' }}>
+                    <View style={{ marginHorizontal: 20 }}>
+                        <Image />
+                        <View style={{ marginBottom: 10 }}>
+                            <Text style={{ fontWeight: 'bold', fontSize: 20, color: 'black' }}>
+                                Use your search skills
+                            </Text>
+                        </View>
+                        <View style={{ marginBottom: 50 }}>
+                            <Text style={{ letterSpacing: -0.2, fontSize: 14, color: 'grey' }} adjustsFontSizeToFit={true} numberOfLines={1}>
+                                We'll get you the products. We'll make shopping easy.
+                            </Text>
+                        </View>
+
+                        <Steps
+                            icon={'ios-document-outline'}
+                            title={'Register yourself'}
+                            description={'Get register on this platform so then you can shop amazing offers and products.'}
+                        />
+                        <Steps
+                            icon={'paper-plane-outline'}
+                            title={'Order the product'}
+                            description={"Order your favorite product online through this app with in your area."}
+                        />
+                        <Steps
+                            icon={'cloud-done-outline'}
+                            title={'Take your Order'}
+                            description={"We'll deliver your order at your door steps and make payment door to door."}
+                        />
                     </View>
 
-                    <Steps
-                        icon={'ios-document-outline'}
-                        title={'Register yourself'}
-                        description={'Get register on this platform so then you can shop amazing offers and products.'}
-                    />
-                    <Steps
-                        icon={'paper-plane-outline'}
-                        title={'Order the product'}
-                        description={"Order your favorite product online through this app with in your area."}
-                    />
-                    <Steps
-                        icon={'cloud-done-outline'}
-                        title={'Take your Order'}
-                        description={"We'll deliver your order at your door steps and make payment door to door."}
-                    />
-
-                    <Button title={'Continue'} style={{ marginTop: 46 }} onPress={() => props.navigation.navigate('SignupModel', { fromSeller: true })} />
+                    {/* <Button title={'Continue'} style={{ marginTop: 46 }} onPress={() => props.navigation.navigate('SignupModel', { fromSeller: true })} /> */}
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', top: -10, marginTop: 60 }}>
+                        <Card style={{ width: '44.4%' }} text={'Continue as User'} image={require('../../assets/images/user.png')} />
+                        <Card style={{ width: '44.4%' }} text={'Continue as Admin'} image={require('../../assets/images/admin.png')} />
+                    </View>
 
                 </ScrollView>
             </View>
