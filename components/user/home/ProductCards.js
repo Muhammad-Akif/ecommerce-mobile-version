d8868be0382110bd5b88a2625b60b4fa7192ff1e
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-12-27 22:11:14 +0500

    Added Redux Libraries & modified Product Cart Item!

--- components/user/home/ProductCards.js
+++ components/user/home/ProductCards.js
@@ -24,36 +24,20 @@ const Card = ({ data }) => {
               >
                 <Text style={{ color: 'green', fontSize: 12 }}>20% OFF</Text>
               </TouchableOpacity>
-              <Image
-                source={{
-                  uri: item.food.image,
-                }}
-                style={styles.productImage}
-              />
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}>
-                <Text
-                  style={{ color: '#494949', fontWeight: '200' }}
-                  onPress={() => {
-                    alert('Title ' + item.title + ' Clicked');
-                  }}>
+              <Image source={{ uri: item.food.image }} style={styles.productImage} />
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Text style={{ color: '#494949', fontWeight: '200', fontFamily: 'text-bold' }} onPress={() => { alert('Title ' + item.food.label + ' Clicked') }} adjustsFontSizeToFit={true} numberOfLines={1}>
                   {item.food.label}
                 </Text>
               </View>
               <View style={styles.childViewTextStyle}>
-                <Text style={{ color: '#606070', fontWeight: '200' }}>
+                <Text style={{ color: 'grey', fontFamily: 'italic', fontSize: 15 }}>
                   {item.food.categoryLabel}
                 </Text>
               </View>
               <Text style={{ color: colors.primary, fontWeight: '200' }}>Rs. {price} <Text style={{ textDecorationLine: 'line-through', color: '#000' }}>Rs. {prePrice}</Text> </Text>
-
-              <TouchableOpacity
-                style={styles.button}
-              >
-                <Text style={{ color: colors.primary }}>Add to cart</Text>
+              <TouchableOpacity style={styles.button}>
+                <Text style={{ color: colors.primary, fontFamily: 'bold' }}>Add to cart</Text>
               </TouchableOpacity>
             </View>
           ))}
@@ -94,7 +78,7 @@ const styles = StyleSheet.create({
   cardStyle: {
     backgroundColor: '#fff',
     paddingVertical: 10,
-    marginTop: 0,
+    marginTop: 0
   },
   card: {
     marginVertical: 10,
@@ -105,8 +89,7 @@ const styles = StyleSheet.create({
     elevation: 5,
     width: 150,
     borderRadius: 15,
-
-    paddingBottom: 0
+    paddingBottom: 8
   },
   productImage: {
     width: '100%',
@@ -115,7 +98,7 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: 'white',
-    height: 35,
+    height: 30,
     borderRadius: 5,
     borderWidth: 0.8,
     marginTop: 5,
--- navigators/MainDrawerNavigator.js
+++ navigators/MainDrawerNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import {
     createDrawerNavigator,
     DrawerContentScrollView,
