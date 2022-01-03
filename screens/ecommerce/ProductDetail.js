6c040b42cf550e97ca1e2d6ff50f57e9ba23c837
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-01-03 14:41:17 +0500

    Implemented Cart Functionality!

--- screens/ecommerce/ProductDetail.js
+++ screens/ecommerce/ProductDetail.js
@@ -3,37 +3,64 @@ import { View, Text, StyleSheet, Image, ScrollView, Button } from 'react-native'
 import colors from '../../constants/colors';
 import { useEcommerceContext } from '../../contexts/ContextProvider';
 import CartItem from '../../models/cartItem';
+import Cart from '../../models/cart';
+import checkAndWriteFile from '../../functions/checkAndWriteFile';
 
 const ProductDetailScreen = props => {
-    const selectedProduct = props?.route?.params?.item;
+    const product = props?.route?.params?.item;
     const category = props?.route?.params?.category;
-    const { cart, setCart } = useEcommerceContext();
+    const { cart, setCart, auth, allData, setAllData } = useEcommerceContext();
 
     useEffect(() => {
         props.navigation.setOptions({
-            headerTitle: selectedProduct.name[0].toUpperCase() + selectedProduct.name.slice(1)
+            headerTitle: product.name[0].toUpperCase() + product.name.slice(1)
         })
     }, [])
 
-    const handleAddToCart = () => {
-        const index = cart.items.findIndex(cartItem => cartItem.id == selectedProduct.id);
+    const handleAddToCart = async () => {
+        const cartIndex = cart.findIndex(cartItem => cartItem.username == auth.username);
+        if (cartIndex == -1) {
+            const newCart = [...cart, new Cart(
+                auth.username,
+                product.price,
+                [new CartItem(product.id, product.name, product.detail, product.price, product.uri, category, 1, product.price)]
+            )]
+            setCart(newCart);
+            const newData = {
+                ...allData,
+                cart: newCart
+            }
+            await checkAndWriteFile(newData);
+            setAllData(newData)
+            return;
+        }
+        const index = cart[cartIndex].items.findIndex(cartItem => cartItem.id == product.id);
         if (index != -1) return;
 
-        const items = [...cart.items, new CartItem(selectedProduct.id, selectedProduct.name, selectedProduct.detail, selectedProduct.price, selectedProduct.uri, category, 1, selectedProduct.price)];
-        setCart({
-            ...cart,
-            items
-        });
+        const newCart = [...cart]
+        newCart.splice(cartIndex, 1, new Cart(
+            auth.username,
+            product.price,
+            [...cart[cartIndex].items, new CartItem(product.id, product.name, product.detail, product.price, product.uri, category, 1, product.price)]
+        ))
+
+        setCart(newCart);
+        const newData = {
+            ...allData,
+            cart: newCart
+        }
+        await checkAndWriteFile(newData);
+        setAllData(newData)
     }
 
     return (
         <ScrollView>
-            <Image style={styles.image} source={{ uri: selectedProduct?.uri }} />
+            <Image style={styles.image} source={{ uri: product?.uri }} />
             <View style={styles.buttonContainer}>
                 <Button color={colors.primary} title='Add to Cart' onPress={handleAddToCart} />
             </View>
-            <Text style={styles.price}>${selectedProduct?.price}</Text>
-            <Text style={styles.description}>{selectedProduct?.detail}</Text>
+            <Text style={styles.price}>${product?.price}</Text>
+            <Text style={styles.description}>{product?.detail}</Text>
         </ScrollView>
     );
 }
