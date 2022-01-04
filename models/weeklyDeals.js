6a20aee82891f193e8a7d6b18b89f884879498c5
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-01-04 21:26:22 +0500

    Updated Modal

--- models/weeklyDeals.js
+++ models/weeklyDeals.js
@@ -1,12 +1,11 @@
 class weeklyDeals {
-    constructor(category, id, name, detail, price, uri, ratings, discountPrice, off) {
+    constructor(id, name, detail, price, uri, quantity, off) {
         this.id = id; // unique & number
         this.name = name; // string
         this.detail = detail; //string
         this.price = price; // float | real price
         this.uri = uri;
         this.quantity = quantity;
-        this.discountPrice = discountPrice; // number
         this.off = off; // string
     }
 }
