12b5b3435484badaea275110af104bfb33d2257c
muhammad-akif <muhammadakif2917@gmail.com>
2022-01-05 21:28:09 +0500

    UPDATED WEEKLY MAIN

--- components/admin/deals/weeklyDeals.js
+++ components/admin/deals/weeklyDeals.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import colors from '../../../constants/colors';
 import AddIcon from 'react-native-vector-icons/AntDesign'
+import DeleteIcon from 'react-native-vector-icons/AntDesign'
 
 
 const weeklyDeals = props => {
@@ -13,10 +14,33 @@ const weeklyDeals = props => {
                 </Text>
             </View>
             <View style={{ height: '93%', justifyContent: 'center', alignItems: 'center' }}>
-                <View style={{ width: '100%', alignItems: 'center' }}>
-                    <Text numberOfLines={1} adjustsFontSizeToFit={true} style={{ fontSize: 30, marginBottom: 10, fontFamily: 'bold' }}>
-                        ohh snap! no offers yet
-                    </Text>
+                <View key={product.id} style={styles.Acard}>
+                    <TouchableOpacity
+                        style={styles.button2}
+                    >
+                        <Text style={{ color: 'green', fontSize: 12 }}>20% OFF</Text>
+                    </TouchableOpacity>
+                    <Image source={{ uri: product.uri }} style={styles.productImage} />
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <Text style={{ color: '#494949', fontWeight: '200', fontFamily: 'text-bold', fontSize: 16.5 }} adjustsFontSizeToFit={true} numberOfLines={1}>
+                            {product.name}
+                        </Text>
+                    </View>
+                    <View style={styles.childViewTextStyle}>
+                        <Text style={{ color: 'grey', fontFamily: 'italic', fontSize: 15 }}>
+                            {product.detail}
+                        </Text>
+                    </View>
+                    <Text style={{ color: colors.primary, fontWeight: '200' }}>Rs. {product.price} <Text style={{ textDecorationLine: 'line-through', color: '#000' }}>Rs. {item.price}</Text> </Text>
+                    <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <TouchableOpacity style={styles.button} 
+                        // onPress={handleDeletePress.bind(null, product.id)}
+                        >
+                            <Text adjustsFontSizeToFit={true} style={{ color: "red", fontFamily: 'bold', margin: 5 }}>
+                                <DeleteIcon name='delete' style={{ fontSize: 40 }} />
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
                 <View style={{ width: '55%' }}>
                     <Text style={{ textAlign: 'center', color: 'grey', fontFamily: 'italic', fontSize: 16 }} numberOfLines={2} adjustsFontSizeToFit={true}>
